290afdf8cce167274f30310ee5c972ee49cce0b1
eunchae <kec0130@gmail.com>
2024-01-14 23:06:40 +0900

    Add prev and next post link

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -3,5 +3,6 @@ import MenuIcon from './menu.svg';
 import CloseIcon from './close.svg';
 import GlobeIcon from './globe.svg';
 import LoadingIcon from './loading.svg';
+import ChevronRightIcon from './chevron_right.svg';
 
-export { Logo, MenuIcon, CloseIcon, GlobeIcon, LoadingIcon };
+export { Logo, MenuIcon, CloseIcon, GlobeIcon, LoadingIcon, ChevronRightIcon };
